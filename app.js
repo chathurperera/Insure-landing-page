4881e8db2c8e43fa067fbaa453e6cfee8324bd9d
chathura <chathuraperera007@gmail.com>
2021-05-29 20:31:31 +0530

    mobile nav

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('ul');
+
+
+hamburger.addEventListener('click' , () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
